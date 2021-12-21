90c048970fa7f07aa59e25f5bf041476d7d931eb
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-12-21 10:27:27 +0700

    Add project files.

---
+++ NVC1521050560/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NVC1521050560
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NVC1521050560/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NVC1521050560.MvcApplication" Language="C#" %>
